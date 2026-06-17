23f0634f51a246b4a9b0776a9f67dcede3fd0b10
Md. Rashadul Alam <rashed.techpro@gmail.com>
2026-06-17 20:21:53 +0600

    Added template for java and python class

---
+++ template/template.java
@@ -0,0 +1,63 @@
+package template;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class template {
+
+    private final boolean debug;
+    private final BufferedReader bufferedReader;
+    private StringTokenizer stringTokenizer;
+
+    public template(boolean debug) {
+        this.debug = debug;
+        this.bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    private void dprint(Object... args) {
+        if (debug) {
+            StringBuilder sb = new StringBuilder();
+            for (Object arg : args) {
+                sb.append(arg).append(" ");
+            }
+            System.out.println(sb.toString().trim());
+        }
+    }
+
+    private String nextToken() throws IOException {
+        while (stringTokenizer == null || !stringTokenizer.hasMoreTokens()) {
+            String line = bufferedReader.readLine();
+            if (line == null)
+                return null;
+            stringTokenizer = new StringTokenizer(line);
+        }
+        return stringTokenizer.nextToken();
+    }
+
+    public void generateOutput() {
+        try {
+            // // Take input from user
+            // dprint("-------------------------------------");
+            // dprint("Input the number.");
+            // String inputNumber = nextToken();
+            // if (inputNumber == null)
+            //     return;
+
+            // int number = Integer.parseInt(inputNumber);
+            // dprint("The number is:", number);
+
+            dprint("-------------------------------------");
+            dprint("Hello Output!");
+            dprint("-------------------------------------");
+        } catch (Exception exception) {
+            if (debug)
+                exception.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new template(true).generateOutput();
+    }
+}
---
+++ template/template.py
@@ -0,0 +1,29 @@
+import sys
+
+class template:
+
+    def __init__(self, debug: bool = False):
+        self.debug = debug
+
+    def dprint(self, *args, **kwargs):
+        if self.debug:
+            print(*args, **kwargs)
+
+    def generateOutput(self):
+        # # Take input from user
+        # self.dprint("-------------------------------------")
+        # self.dprint("Input the number.")
+        # inputNumber = sys.stdin.readline()
+        # if not inputNumber:
+        #     return
+
+        # number = int(inputNumber.strip())
+        # self.dprint(f"The number is: {number}")
+
+        # Print hello output
+        self.dprint("-------------------------------------")
+        self.dprint("Hello Output!")
+        self.dprint("-------------------------------------")
+
+if __name__ == "__main__":
+    template(True).generateOutput()
